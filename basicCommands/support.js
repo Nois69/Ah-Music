7813820498067084fd7563ceb04bf40c8efa8933
Nois69 <116580992+Nois69@users.noreply.github.com>
2024-04-26 23:34:04 +0600

    Update support.js

--- basicCommands/support.js
+++ basicCommands/support.js
@@ -4,12 +4,12 @@ module.exports = {
     name: 'support',
     description: 'support server of this Bot',
     execute(message, args) {
-        const supportServerLink = 'https://discord.gg/FUEHs7RCqz';
+        const supportServerLink = 'https://discord.gg/aGeeMwkJ2b';
         const embed = new EmbedBuilder()
             .setColor('#FFFFFF')
             .setTitle('Support server')
             .setDescription(`Click [here](${supportServerLink}) to join our server.\nWe will be there for you anytime ❤️`)
-            .setThumbnail(`https://cdn.discordapp.com/attachments/1113800537402527903/1119995338359586837/a_e8d3128cee7f07ccec68962a8294f43d.gif`)
+          
             .setTimestamp();
 
 
